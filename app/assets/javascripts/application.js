9444a32aff3a2c8d7dda682a592a76e20edfa144
mohammed hamza <mohammedh566@gmail.com>
2016-05-10 14:47:26 +0000

    Code base for Bangkok LLC project

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,26 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery_ujs
+//= require jquery
+//= require turbolinks
+//= require bootstrap-hover-dropdown.min
+//= require bootstrap.min
+//= require color-switcher
+//= require custom
+//= require jquery.card
+//= require jquery.dragtable
+//= require jquery.mb.YTPlayer.min
+//= require owl.carousel.min
+//= require SmoothScroll
+//= require twitterFetcher_min
+
